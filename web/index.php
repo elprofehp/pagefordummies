7fc486455d5df407ad427ba776b4f9153b8b41cd
elprofehp <elprofehp@gmail.com>
2019-05-09 17:01:12 -0500

    Testing url handler index.php

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/otherURL', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
 $app->run();
